6112ac01815a596da8c1c721b85a9c75bef99d7e
yousef <gheibiyousef2000@gmail.com>
2022-08-15 21:21:40 +0430

    implement movies and movie detail page

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from 'src/app/services/movie.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.page.scss'],
 })
 export class MovieDetailsPage implements OnInit {
-
-  constructor() { }
+  movie = null;
+  imageBaseUrl = environment.images;
+  constructor(private router: ActivatedRoute, private movieService: MovieService) { }
 
   ngOnInit() {
+    const id = this.router.snapshot.paramMap.get('id');
+    this.movieService.getMovieDetails(id).subscribe((res)=>{
+      this.movie = res;
+      console.log(res);
+    });
   }
 
+  openHomepage(){
+    window.open(this.movie.homepage);
+  }
 }
--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent, LoadingController } from '@ionic/angular';
+import { MovieService } from 'src/app/services/movie.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-movies',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies.page.scss'],
 })
 export class MoviesPage implements OnInit {
+  moives = [];
+  currentPage = 1;
+  imageBaseUrl  = environment.images;
 
-  constructor() { }
+  constructor(private movieService: MovieService,private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
+    this.loadMovies();
   }
 
+  async loadMovies(event?: InfiniteScrollCustomEvent){
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading..',
+      spinner: 'bubbles',
+    });
+    await loading.present();
+    this.movieService.getTopRatedMovies(this.currentPage).subscribe((res)=>{
+      loading.dismiss();
+      // this.moives = [...this.moives, ...res.results];
+      this.moives.push(...res.results);
+      event?.target.complete();
+
+      if(event){
+        event.target.disabled = res.total_pages === this.currentPage;
+      }
+    });
+  }
+
+  loadMore(event: InfiniteScrollCustomEvent){
+    this.currentPage++;
+    this.loadMovies();
+  }
 }
